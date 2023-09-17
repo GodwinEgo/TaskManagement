962600e5076c102c86c55faf1c918c085bf9ac5c
GodwinEgo <godwinehile04@gmail.com>
2023-09-18 00:29:43 +0100

    express

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
