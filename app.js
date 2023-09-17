ab6cd8947da72c54ccd93045d833d52e090640a9
GodwinEgo <godwinehile04@gmail.com>
2023-09-18 00:39:01 +0100

    APP

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-mongoose.connect(process.env.MONGO_URL, () => {
-  console.log(`Connected to MongoDB`);
-});
+//MONGODB CONNECTION
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Error connecting to db", error);
+  });
