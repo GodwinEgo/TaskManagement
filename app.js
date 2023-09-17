f515a12bebf017cf4830e5696bbe55e1152acb45
GodwinEgo <godwinehile04@gmail.com>
2023-09-18 00:43:16 +0100

    cls

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
+app.use(express.json());
+app.use(cors());
 
 //MONGODB CONNECTION
 mongoose
@@ -22,3 +24,13 @@ mongoose
   .catch((error) => {
     console.log("Error connecting to db", error);
   });
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+//SERVER RUNNING
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
