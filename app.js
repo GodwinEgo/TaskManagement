2c178bcb5c1a73f86d9d4669c16f279e6008c5a4
GodwinEgo <godwinehile04@gmail.com>
2023-09-18 15:58:09 +0100

    register

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+const User = require("./models/User");
+const Task = require("./models/Task");
+const passport = require("passport");
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
+
+//MIDDLEWARE FOR SESSION MANAGEMENT
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//INITIALIZE PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+
+//PASSPORT LOCAL STRATEGY FOR AUTHENTICATION
 
 //MONGODB CONNECTION
 mongoose
---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userShcema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userShcema);
