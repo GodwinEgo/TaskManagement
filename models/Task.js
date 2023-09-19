f6d5eea3483d1787ac88d94f5976b943d3c2f7a5
GodwinEgo <godwinehile04@gmail.com>
2023-09-19 21:58:04 +0100

    task

--- models/Task.js
+++ models/Task.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  title: String,
+  title: {
+    type: String,
+    required: true,
+  },
   description: String,
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  dueDate: Date,
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    default: "pending",
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
