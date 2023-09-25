9e554dd45a30f96eccc45eb64e94416bea66f37d
GodwinEgo <godwinehile04@gmail.com>
2023-09-25 21:11:32 +0100

    route

--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -6,4 +6,18 @@ const TaskController = require("../controllers/TaskController");
 const isAuthenticated = require("../middleware/Auth");
 
 //CREATE A NEW TASK
-router.post("/tasks", isAuthenticated, TaskController.cre);
+router.post("/tasks", isAuthenticated, TaskController.createTask);
+
+//GET ALL TASKS FOR AUTHENTICATED USER
+router.get("/tasks", isAuthenticated, TaskController.getAllTasks);
+
+//GET A SINGLE TASK BY ID
+router.get("/tasks/:taskId", isAuthenticated, TaskController.getTaskById);
+
+//UPDATE A TASK BY ID
+router.put("/tasks/:taskId", isAuthenticated, TaskController.updateTask);
+
+//DELETE A TASK BY ID
+router.delete("/tasks/:taskId", isAuthenticated, TaskController.deleteTask);
+
+module.exports = router;
